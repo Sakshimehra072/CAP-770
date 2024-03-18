fee441feb79dc9ab813b39a0410f49cfa8f0e146
Sakshimehra072 <151001309+Sakshimehra072@users.noreply.github.com>
2024-03-18 23:05:24 +0530

    Update 1primenumber.cpp

--- 1primenumber.cpp
+++ 1primenumber.cpp
@@ -1,10 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-int n= 20;
-int arr[n];
 
-for(int i=0 ; i<n; i++){
- cout<<arr[i];
+bool isPrime(int number) {
+    if (number <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= number; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
+
+int main() {
+    int n;
+    cout << "Enter the range: ";
+    cin >> n;
+    cout << "Prime numbers up to " << n << " are: ";
+    for (int i = 2; i <= n; i++) {
+        if (isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    return 0;
 }
